3b7620bb0a1df1cab71014ff18af2d55902529b9
Chris Spring <chris@nolaspring.net>
2017-08-09 22:45:42 -0400

    rbenv and dep checks

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,43 @@
 #!/bin/bash
 
-echo 'todo: shim in our shell using virtualenv'
+echo 'todo: shim in our shell using rbenv'
 echo 'todo: configure agnsotic docker container location settings'
 
+echo 'todo: list packages, grep for vagant, git and docker.'
+echo 'todo: if we dont find any, bounce'
 
 #make sure we load up vagrant with relavant plugins
 vagrant plugin install vagrant-env
 
 #purge the .env file used by vagrant
-
-#shove these in an .env file for Vagrant
-echo ROOT_PATH='~/' >> .env
-echo COOKBOOK_PATH='cookbooks' >> .env
-echo DATA_BAGS_PATH='data_bags' >> .env
-
-echo ROLES_PATH='roles' >> .env
-echo NODES_PATH='nodes' >> .env
-
-
-mkdir ~/px4-system
-pushd ~/px4-system
+mkdir $PWD/working
+pushd $PWD/working
 mkdir cookbooks
 mkdir data_bags
 mkdir roles
 mkdir nodes
 
+#clone cookbooks for provisioning node
+pushd cookbooks
+git clone https://github.com/chef/ohai.git
+git clone https://github.com/chef/chef.git
+git clone https://github.com/dev-sec/chef-os-hardening
+
+
+#build out some basic tools to implement automation on the provisoning node
+git clone https://supermarket.chef.io/cookbooks/nodejs
+git clone https://gitlab.com/s4m-chef-repositories/docker-platform.git
+git clone https://github.com/chef-partners/google-compute-engine.git
+
+echo 'todo:create cookbook for processIO'
+
+
+
+
+#set environment parameters for vagrant to build out the provisioning node
+echo COOKBOOK_PATH='cookbooks' >> .env
+echo DATA_BAGS_PATH='data_bags' >> .env
+echo ROLES_PATH='roles' >> .env
+echo NODES_PATH='nodes' >> .env
 #start vagrant and 1. build a docker container and run chef-commands to build out our provisioning node
 vagrant up
